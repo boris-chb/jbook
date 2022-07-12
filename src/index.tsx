18f29582b2b7c4d02d8a0d9826ba27e77807a0b3
Boris <boris.chobyrka@gmail.com>
2022-07-13 00:14:17 +0100

    chore: refactor App component (index.tsx), split into CodeCell.jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-import CodeEditor from './components/CodeEditor';
-import Preview from './components/Preview';
-
-import bundle from './bundler';
+import CodeCell from './components/CodeCell';
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
-    <div>
-      <CodeEditor
-        initialValue={"'initial value from index.tsx'"}
-        onChange={value => {
-          setInput(value);
-        }}
-      />
-      <div>
-        <button onClick={onClick}>Run</button>
-      </div>
-      <Preview code={code} />
-    </div>
+    <>
+      <CodeCell />
+      <CodeCell />
+    </>
   );
 };
 
